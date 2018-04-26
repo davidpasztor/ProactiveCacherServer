0e57adf7ee0e46a4df5e8840b0ccd28b10f0702a
David Pasztor <pasztor.david95@hotmail.com>
2018-04-26 18:00:27 +0200

    Function added for generating the predicted ratings for all users based on the current ratings using the likely package for collaborative filtering

--- CacheManager.js
+++ CacheManager.js
@@ -1,5 +1,6 @@
 const apn = require('apn');
 const fs = require('fs');
+const Recommender = require('likely');
 
 const options = {
   token: {
@@ -69,12 +70,67 @@ function pushVideoToDevice(videoID,deviceToken){
     });
 }
 
-  
+// Make a caching decision for the specific user - decide what content to push
+// to the device (if any) and when to push it
+// This function should be called every time a new AppUsageLog object is uploaded
+// 
+function makeCachingDecision(user){
+	// Predict when the user will use the app the next time
+	// Fetch the AppUsageLog object from the previous day closest to, but later
+	// than the current time and find the last UserLog where the user had wifi
+	// connection --> content pushing needs to happen at that point in time
+	
+    // Find the recommended movies for the user
+	//var model = generatePredictedRatings(users,videos,currentRatings);
+	//var recommendations = model.recommendations(user.userID);
+}
+
+// Make a caching decision for a specific user - simply predict the next time 
+// they'll have wifi access and push a random video that they haven't cached yet
+function makeCachingDecisionsV0(user){
+	// UserLogs to use for network availability checking
+	let wifiAvailabilityLogs = user.logs.filter('networkStatus == "WiFi"');
+	// AppUsageLogs to predict the number of videos needed to be pushed and the
+	// time of pushing (only care about logs where the user watched any videos)
+	let appUsageLogs = user.appLogs.filter("watchedVideosCount > 0");
+}
+
+// Helper function for creating an n-by-m matrix (2D array)
+function createMatrix(n,m){
+	var matrix = new Array(n);
+	for (var i=0;i<n;i++){
+		matrix[i] = new Array(m);        
+	}
+	return matrix;
+}
+
+// Generate the predicted ratings for all users
+function generatePredictedRatings(users,videos,currentRatings){
+    // Create the rowLabels (userID)
+	let rowLabels = users.map(user => user.userID);
+    // Create the column labels (videoID)
+	let columnLabels = videos.map(video => video.youtubeID);
+
+    // Create the input matrix in the form that rows represent users and columns 
+    // represent ratings for a specific movie by each user
+	var ratingsMatrix = createMatrix(rowLabels.length,columnLabels.length);
+	ratingsMatrix.forEach(row => row.fill(0));  // likely needs 0s for the non-rated videos
+	currentRatings.forEach(rating => {
+		ratingsMatrix[rowLabels.indexOf(rating.user.userID)][columnLabels.indexOf(rating.video.youtubeID)] = rating.score        
+	});
+	console.log("Existing ratings:");
+	console.log(ratingsMatrix);
+
+    // Build the model
+    var model = Recommender.buildModel(ratingsMatrix, rowLabels, columnLabels);
+	return model; 
+}
 
 module.exports = {
     sendNetworkAvailabilityReqPush,
 	sendNetwAvailReqPushToAll,
     pushVideoToDevice,
-	userLogRequestInterval
+	userLogRequestInterval,
+	generatePredictedRatings
 }
 
